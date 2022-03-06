2dfdded5163f9489d1f6335a6830199ffb4ebf9e
Athira-C-P <athiracp4422@gmail.com>
2022-03-07 01:36:10 +0530

    Added program snake and ladder game played with single player at start position 0

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder Problem");
+
+        int startPosition = 0;
+        System.out.println("Game starts from : " + startPosition);
+    }
+}
